bf0003c3fe01ab2e9d96609a27ecb5e3442ad385
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2019-04-23 09:13:08 +0200

    Correct way of inheriting user related forms

--- bupit/users/forms.py
+++ bupit/users/forms.py
@@ -3,6 +3,23 @@ from crispy_forms.layout import Submit
 from django.contrib.auth import forms, get_user_model
 
 
+"""
+The following forms are compatible with any subclass of AbstractBaseUser:
+
+AuthenticationForm: Uses the username field specified by USERNAME_FIELD.
+SetPasswordForm
+PasswordChangeForm
+AdminPasswordChangeForm
+The following forms make assumptions about the user model and can be used as-is if those assumptions are met:
+
+PasswordResetForm: Assumes that the user model has a field that stores the user’s email address with the name returned by get_email_field_name() (email by default) that can be used to identify the user and a boolean field named is_active to prevent password resets for inactive users.
+Finally, the following forms are tied to User and need to be rewritten or extended to work with a custom user model:
+
+UserCreationForm
+UserChangeForm
+"""
+
+
 class RegisterUserForm(forms.UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
@@ -10,6 +27,10 @@ class RegisterUserForm(forms.UserCreationForm):
         self.helper.form_action = 'user-create'
         self.helper.add_input(Submit("submit", "Submit"))
 
-    class Meta:
-        fields = ('username',)
+    class Meta(forms.UserCreationForm.Meta):
         model = get_user_model()
+        # fields = UserCreationForm.Meta.fields + ('my_fields',)
+
+
+class ChangeUserForm(forms.UserChangeForm):
+    pass
